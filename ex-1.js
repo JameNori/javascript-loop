a5beb968acb39321e8e305fe9a956056c0c361b0
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-07-21 13:27:25 +0700

    complete exercise 1-6

--- ex-1.js
+++ ex-1.js
@@ -2,3 +2,9 @@
 let scores = [89, 70, 60, 50, 100, 71];
 
 // Start coding here
+
+// Decrease student score by 10
+for (let i = 0; i < scores.length; i++) {
+  scores[i] = scores[i] - 10;
+}
+console.log(scores);
